94c91116650848b1aa44e437e2e46830ae844b3c
iShawyha <shawyhaf@gmail.com>
2026-04-23 18:22:14 +0300

    fix packaging and update vesrion

--- flake.nix
+++ flake.nix
@@ -8,41 +8,77 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        sysData = {
+          "x86_64-linux" = {
+            url = "https://github.com/OlexiyOdarchuk/Student-Hryvnia-Miner/releases/download/v1.2.0/SHMiner-linux-amd64";
+            hash = "sha256-440KHKVJgNguFsd9zoz/YdYh1CK9jznZ8pLCcxC7GC8=";
+            isDmg = false;
+          };
+          "x86_64-darwin" = {
+            url = "https://github.com/OlexiyOdarchuk/Student-Hryvnia-Miner/releases/download/v1.2.0/SHMiner-darwin-amd64.dmg";
+            hash = "sha256-/ZZzu8NWOlYi5XNW7yTk9ulPQ9jrPhIRaT5MV9dY1mM=";
+            isDmg = true;
+            appName = "SHMiner-darwin-amd64.app";
+          };
+          "aarch64-darwin" = {
+            url = "https://github.com/OlexiyOdarchuk/Student-Hryvnia-Miner/releases/download/v1.2.0/SHMiner-darwin-arm64.dmg";
+            hash = "sha256-40L3/K/eYEaMT6+8qjoCY0rzIo/5zzZ5q8l9HiRp808=";
+            isDmg = true;
+            appName = "SHMiner-darwin-arm64.app";
+          };
+        };
+
+        target = sysData.${system} or null;
       in {
-        packages.shminer = pkgs.stdenv.mkDerivation (finalAttrs: {
+        packages.shminer = if target == null then 
+          pkgs.writeScriptBin "shminer-unsupported" "echo 'Platform ${system} is not supported by prebuilt binaries.'" 
+        else pkgs.stdenv.mkDerivation (finalAttrs: {
           pname = "shminer";
-          version = "1.1.3";
+          version = "1.2.0";
 
           src = pkgs.fetchurl {
-            url = "https://github.com/OlexiyOdarchuk/Student-Hryvnia-Miner/releases/download/v${finalAttrs.version}/SHMiner-linux-amd64";
-            hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            url = target.url;
+            hash = target.hash;
           };
 
-          nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+          nativeBuildInputs = [ pkgs.makeWrapper ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.autoPatchelfHook pkgs.wrapGAppsHook3 ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.undmg ];
 
-          buildInputs = with pkgs; [
+          buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
             webkitgtk_4_1
             gtk3
-            cairo
-            gdk-pixbuf
-            glib
-            pango
-          ];
+            libGL
+            gsettings-desktop-schemas
+          ]);
 
-          dontUnpack = true;
+          dontUnpack = !target.isDmg;
 
-          installPhase = ''
+          installPhase = if pkgs.stdenv.isLinux then ''
             mkdir -p $out/bin
-            cp $src $out/bin/shminer
-            chmod +x $out/bin/shminer
+            cp $src $out/bin/.shminer-raw
+            chmod +x $out/bin/.shminer-raw
+
+            makeWrapper $out/bin/.shminer-raw $out/bin/shminer \
+              --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH" \
+              --set WEBKIT_DISABLE_COMPOSITING_MODE "0" \
+              --set WEBKIT_FORCE_COMPOSITING_MODE "1" \
+              --prefix LD_LIBRARY_PATH : "/usr/lib:/usr/lib32"
+          '' else ''
+            mkdir -p "$out/Applications"
+            cp -r "${target.appName}" "$out/Applications/"
+            mkdir -p $out/bin
+            ln -s "$out/Applications/${target.appName}/Contents/MacOS/SHMiner" $out/bin/shminer
           '';
 
           meta = with pkgs.lib; {
             description = "Mining Client for S-UAH cryptocurrency";
             homepage = "https://github.com/OlexiyOdarchuk/Student-Hryvnia-Miner";
             license = licenses.gpl3;
-            platforms = platforms.linux;
+            platforms = builtins.attrNames sysData;
           };
         });
 
